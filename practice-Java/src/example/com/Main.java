e4c9cca73ad3cb31f2279b10938b5877fc371ab8
KanaKarimata <kana.lt22@gmail.com>
2022-12-22 09:07:29 +0900

    First Commit

---
+++ practice-Java/src/example/com/Main.java
@@ -0,0 +1,12 @@
+package example.com;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] array = new int[3];
+//        Class clazz = array.getClass();
+//        System.out.println(clazz.getCanonicalName());
+
+        int[] array = new int[0];
+        System.out.println(array);
+    }
+}
